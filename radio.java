59ed332945810caf56a22be74f1fabbdaade77ae
Alvaco <vas09284@uvg.edu.gt>
2017-01-18 07:45:15 -0600

    JRadio and radio

--- radio.java
+++ radio.java
@@ -1,191 +1,71 @@
-import static java.lang.System.out;
-public class radio {
-private double frecuencia;
-private boolean estado ;
-private double[] seleccionar;
-private double[] cambiar;
-private double emisora;
-private boolean encendido;
-private boolean modulacion;
-private double[] seleccionaram;
-private double[] seleccionarpm;
-private double avanzar;
-
-/**
- * @return the modulacion
- */
-public double isfrecuencia() {
- return frecuencia;
-}
-/**
- * @param modulacion the modulacion to set
- */
-public void setFrecuencia(double seleccionar2) {
- this.frecuencia = seleccionar2;
-}
-/**
- * @return the oNOFF
- */
-public boolean isONOFF() {
- return estado;
-}
-/**
- * @param oNOFF the oNOFF to set
- */
-public void setONOFF(boolean oNOFF) {
- estado = oNOFF;
-}
-/**
- * @return the seleccionaram
- */
-public double[] getSeleccionaram() {
- return getSeleccionaram();
-}
-/**
- * @param seleccionaram the seleccionaram to set
- */
-public void setSeleccionaram(double[] seleccionaram) {
- this.seleccionaram = seleccionaram;
-}
-/**
- * @return the seleccionarpm
- */
-public double[] getSeleccionarpm() {
- return getSeleccionarpm();
-}
-/**
- * @param seleccionarpm the seleccionarpm to set
- */
-public void setSeleccionarpm(double[] seleccionarpm) {
- this.seleccionarpm = seleccionarpm;
-}
-/**
- * @return the cambiar
- */
-public double[] getCambiar() {
- return cambiar;
-}
-/**
- * @param cambiar the cambiar to set
- */
-public void setCambiar(double[] cambiar) {
- this.cambiar = cambiar;
-}
-/**
- * @return the estado
- */
-public boolean isEstado() {
- return estado;
-}
-/**
- * @param estado the estado to set
- */
-public void setEstado(boolean estado) {
- this.estado = estado;
-}
-/**
- * @return the emisora
- */
-public double getEmisora1() {
- return emisora;
-}
-/**
- * @param emisora the emisora to set
- */
-public void setEmisora(double emisora) {
- this.emisora = emisora;
-}
-public boolean isEncendido() {
- return isEncendido();
-}
-public void estado() {
- encendido = !encendido;
- out.println(encendido ? "Radio Encendida" : "Radio Apagada");
- out.println(encendido ? "Frecuencia seleccionada: " + getEmisora1() : "");
-}
-public double frecuencia() {
- modulacion = !modulacion;
- if (modulacion) {
-  seleccionar[1] = frecuencia();
-  setFrecuencia(seleccionar[0]);
-  out.println("FM");
- } else {
-  seleccionar[0] = frecuencia();
-  setFrecuencia(seleccionar[1]);
-  out.println("AM");
- }
-}
-public void cambiar(boolean avanzar) {
- if (!encendido) {
-  out.println("El radio se encuentra apagado...");
-  return;
- }
- if (avanzar) {
-  if (modulacion) {
-   if (frecuencia() == FM_FIN) {
-    setFrecuencia(FM_INI);
-   } else {
-    setFrecuencia(frecuencia() + FM_CAMBIO);
-   }
-  } else {
-   if (frecuencia() == AM_FIN) {
-    setFrecuencia(AM_INI);
-   } else {
-    setFrecuencia(frecuencia() + AM_CAMBIO);
-   }
+public interface Radio {
+// Constantes de la radio
+   /**
+    * Cambio en estaciones AM
+    */
+   final int AM_CAMBIO = 10;
+   
+   /**
+    * Cambio en estaciones FM
+    */
+   final double FM_CAMBIO = 0.20;
+   
+   /**
+    * Estacion inicial AM
+    */
+   final int AM_INI = 530;
+   
+   /**
+    * Estacion final AM
+    */
+   final int AM_FIN = 1610;
+   
+   /**
+    * Estacion inicial FM
+    */
+   final double FM_INI = 87.90;
+   
+   /**
+    * Estacion final FM
+    */
+   final double FM_FIN = 107.90;
+   
+   // Métodos de la radio
+   /**
+    * Apaga o enciende la radio
+    */
+   public void ON_OFF();
+   
+   /**
+    * Selecciona entre AM y FM
+    */
+   public void AM_FM();
+   /**
+    * 
+    * @param avance True: Avanza hacia adelante
+    */
+   public void Cambio_de_Emisora(boolean avance);
+   
+   /**
+    * 
+    * @param index Posicion de la estacion a guardar
+    */
+   public void Seleccionar_emisora_guardada(int index);
+   
+   /**
+    * 
+    * @param index Posicion de la estacion guardada
+    */
+   public void Guardar_Emisora(int index);
+   
+   /**
+    * 
+    */
+   public void Guardar_seleccion();
+   
+   /**
+    * 
+    * @return La frecuencia seleccionada (FM o AM)
+    */
+   public double getEmisora();
   }
- } else {
-  if (modulacion) {
-   if (frecuencia() == FM_INI) {
-    setFrecuencia(FM_FIN);
-   } else {
-    setFrecuencia(frecuencia() - FM_CAMBIO);
-   }
-  } else {
-   if (frecuencia() == AM_INI) {
-    setFrecuencia(AM_FIN);
-   } else {
-    setFrecuencia(frecuencia() - AM_CAMBIO);
-   }
-  }
- }
- out.println(encendido ? "Radio Encendida" : "Radio Apagada");
- out.println(modulacion ? "Modulacion FM" : "Modulacion AM");
- out.println("Frecuencia seleccionada: " + getEmisora1());
-}
-public void seleccionar(int index) {
- if (!encendido) {
-  out.println("El radio se encuentra apagado...");
-  return;
- }
- if (modulacion) {
-  setFrecuencia(fav_fm[index]);
- } else {
-  setFrecuencia(fav_am[index]);
- }
-}
-public void guardar(int index) {
- if (!encendido) {
-  out.println("El radio se encuentra apagado...");
-  return;
- }
- if (modulacion) {
-  fav_fm[index] = frecuencia();
- } else {
-  fav_am[index] = frecuencia();
- }
-}
-@Override
-public void Guardar_seleccion() {
- if (!encendido) {
-  out.println("El radio se encuentra apagado...");
-  return;
- }
-}
-@Override
-public double getEmisora() {
- return frecuencia();
-}
-}
-}
-}
